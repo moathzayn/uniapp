55fd415252c09997503f4f92ba7d35cc423098c5
Moath Zayn <101884237+moathzayn@users.noreply.github.com>
2024-06-19 19:26:06 +0300

    post to firebase database

--- lib/screens/add_post_screen.dart
+++ lib/screens/add_post_screen.dart
@@ -5,6 +5,7 @@ import 'package:image_picker/image_picker.dart';
 import 'package:provider/provider.dart';
 import 'package:uniapp/Providers/user_provider.dart';
 import 'package:uniapp/models/user.dart';
+import 'package:uniapp/resources/firestore_methods.dart';
 import 'package:uniapp/uitls/utils.dart';
 
 class AddPostScreen extends StatefulWidget {
@@ -16,55 +17,136 @@ class AddPostScreen extends StatefulWidget {
 
 class _AddPostScreenState extends State<AddPostScreen> {
   Uint8List? _file;
+  final TextEditingController _descriptionController = TextEditingController();
+  bool _isLoading = false;
+  void postImage(
+    String uid,
+    String username,
+    String profileImage,
+  ) async {
+    setState(() {
+      _isLoading = true;
+    });
+    try {
+      String res = await FirestoreMethods().uploadPost(
+          _descriptionController.text, _file!, uid, username, profileImage);
+      if (res == "success") {
+        setState(() {
+          _isLoading = false;
+        });
+        showSnackBar('Posted', context);
+        clearImage();
+      } else {
+        setState(() {
+          _isLoading = false;
+        });
+        showSnackBar(res, context);
+      }
+    } catch (e) {
+      showSnackBar(e.toString(), context);
+    }
+  }
+
   _selectImage(BuildContext context) async {
     return showDialog(
-        context: context,
-        builder: (context) {
-          return SimpleDialog(
-            title: const Text('Create Post'),
-            children: [
-              SimpleDialogOption(
-                padding: const EdgeInsets.all(20),
-                child: const Text('Take a Photo'),
-                onPressed: () async {
-                  Navigator.of(context).pop();
-                  Uint8List file = await pickImage(ImageSource.camera);
-                  setState(() {
-                    _file = file;
-                  });
-                },
+      context: context,
+      builder: (context) {
+        return SimpleDialog(
+          title: const Text('Create Post'),
+          children: [
+            SimpleDialogOption(
+              padding: const EdgeInsets.all(20),
+              child: const Row(
+                children: [
+                  Icon(Icons.camera),
+                  Text('Take a Photo'),
+                ],
               ),
-              SimpleDialogOption(
-                padding: const EdgeInsets.all(20),
-                child: const Text('Chose From Gallery'),
-                onPressed: () async {
-                  Navigator.of(context).pop();
-                  Uint8List file = await pickImage(ImageSource.gallery);
-                  setState(() {
-                    _file = file;
-                  });
-                },
-              )
-            ],
-          );
-        });
+              onPressed: () async {
+                Navigator.of(context).pop();
+                Uint8List file = await pickImage(ImageSource.camera);
+                setState(() {
+                  _file = file;
+                });
+              },
+            ),
+            SimpleDialogOption(
+              padding: const EdgeInsets.all(20),
+              child: const Row(
+                children: [
+                  Icon(Icons.photo_sharp),
+                  Text('Chose From Gallery'),
+                ],
+              ),
+              onPressed: () async {
+                Navigator.of(context).pop();
+                Uint8List file = await pickImage(ImageSource.gallery);
+                setState(() {
+                  _file = file;
+                });
+              },
+            ),
+            SimpleDialogOption(
+              padding: const EdgeInsets.all(20),
+              child: const Row(
+                children: [
+                  Icon(Icons.cancel),
+                  Text('Cancel'),
+                ],
+              ),
+              onPressed: () async {
+                Navigator.of(context).pop();
+              },
+            )
+          ],
+        );
+      },
+    );
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    _descriptionController.dispose();
+  }
+
+  void clearImage() {
+    setState(() {
+      _file = null;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     final User user = Provider.of<UserProvider>(context).getUser;
     return _file == null
-        ? Center(
-            child: TextButton(
-            onPressed: () => _selectImage(context),
-            child: const Text('Post'),
-          ))
+        ? Scaffold(
+            appBar: AppBar(
+              title: const Text('Post'),
+              actions: const [
+                Padding(
+                  padding: EdgeInsets.only(right: 15.0),
+                  child: Text(
+                    'Post',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ],
+            ),
+            body: Center(
+                child: TextButton(
+              onPressed: () => _selectImage(context),
+              child: const Text('Select Photo'),
+            )),
+          )
         : Scaffold(
             appBar: AppBar(
               title: const Text('Post'),
               actions: [
                 TextButton(
-                  onPressed: () {},
+                  onPressed: () =>
+                      postImage(user.uid, user.username, user.photoUrl),
                   child: const Text(
                     'Post',
                     style: TextStyle(fontWeight: FontWeight.bold),
@@ -72,87 +154,89 @@ class _AddPostScreenState extends State<AddPostScreen> {
                 )
               ],
             ),
-            body: Padding(
-              padding: EdgeInsetsDirectional.fromSTEB(0, 0, 0, 10),
-              child: Column(
-                mainAxisSize: MainAxisSize.max,
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Row(
+            body: Column(
+              children: [
+                _isLoading
+                    ? const LinearProgressIndicator()
+                    : const Padding(
+                        padding: EdgeInsets.only(top: 2),
+                      ),
+                Padding(
+                  padding: const EdgeInsetsDirectional.fromSTEB(0, 0, 0, 10),
+                  child: Column(
                     mainAxisSize: MainAxisSize.max,
                     mainAxisAlignment: MainAxisAlignment.start,
                     children: [
-                      Padding(
-                        padding: EdgeInsetsDirectional.fromSTEB(10, 10, 5, 0),
-                        child: Container(
-                          width: 50,
-                          height: 50,
-                          clipBehavior: Clip.antiAlias,
-                          decoration: const BoxDecoration(
-                            shape: BoxShape.circle,
+                      Row(
+                        mainAxisSize: MainAxisSize.max,
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsetsDirectional.fromSTEB(
+                                10, 10, 5, 0),
+                            child: Container(
+                              width: 50,
+                              height: 50,
+                              clipBehavior: Clip.antiAlias,
+                              decoration: const BoxDecoration(
+                                shape: BoxShape.circle,
+                              ),
+                              child: Image.network(
+                                user.photoUrl,
+                                fit: BoxFit.cover,
+                              ),
+                            ),
                           ),
-                          child: Image.network(
-                            user.photoUrl,
+                          const Padding(
+                            padding:
+                                EdgeInsetsDirectional.fromSTEB(0, 10, 0, 0),
+                            child: Text(
+                              'You post as ',
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsetsDirectional.fromSTEB(
+                                2, 10, 0, 0),
+                            child: Text(
+                              user.username,
+                              style:
+                                  const TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                        ],
+                      ),
+                      Padding(
+                        padding:
+                            const EdgeInsetsDirectional.fromSTEB(0, 10, 0, 0),
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(0),
+                          child: Image.memory(
+                            _file!,
+                            width: double.infinity,
+                            height: 200,
                             fit: BoxFit.cover,
                           ),
                         ),
                       ),
-                      const Padding(
-                        padding: EdgeInsetsDirectional.fromSTEB(0, 10, 0, 0),
-                        child: Text(
-                          'You post as ',
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width * 1,
+                        child: Padding(
+                          padding: const EdgeInsets.only(left: 5, right: 5),
+                          child: TextField(
+                            controller: _descriptionController,
+                            decoration: const InputDecoration(
+                              hintText: 'Write a caption',
+                              border: InputBorder.none,
+                            ),
+                            maxLines: 3,
+                          ),
                         ),
-                      ),
-                      Padding(
-                        padding:
-                            const EdgeInsetsDirectional.fromSTEB(2, 10, 0, 0),
-                        child: Text(
-                          user.username,
-                          style: const TextStyle(fontWeight: FontWeight.bold),
-                        ),
-                      ),
+                      )
                     ],
                   ),
-                  Padding(
-                    padding: const EdgeInsetsDirectional.fromSTEB(0, 10, 0, 0),
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(0),
-                      child: Image.memory(
-                        _file!,
-                        width: double.infinity,
-                        height: 200,
-                        fit: BoxFit.cover,
-                      ),
-                    ),
-                  ),
-                  SizedBox(
-                    width: MediaQuery.of(context).size.width * 1,
-                    child: const TextField(
-                      decoration: InputDecoration(
-                        hintText: 'Write a caption',
-                        border: InputBorder.none,
-                      ),
-                      maxLines: 3,
-                    ),
-                  )
-                ],
-              ),
-            )
-
-            /*Column(
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    CircleAvatar(
-                      backgroundImage: NetworkImage(user.photoUrl),
-                    ),
-                    
-                  ],
-                )
+                ),
               ],
-            ),*/
-            );
+            ),
+          );
   }
 }
