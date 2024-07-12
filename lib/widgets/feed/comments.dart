b023379156c9809c6f9dd18e8a704144447f522e
Moath Zayn <101884237+moathzayn@users.noreply.github.com>
2024-07-12 16:15:53 +0300

    fix comments screen and make it goes up with keyboard

--- lib/widgets/feed/comments.dart
+++ lib/widgets/feed/comments.dart
@@ -31,97 +31,103 @@ class _CommentsState extends State<Comments> {
         showModalBottomSheet<void>(
           isScrollControlled: true,
           useSafeArea: true,
-          shape: const Border(top: BorderSide.none),
           context: context,
-          builder: (BuildContext context) {
-            return SafeArea(
-              child: Column(
-                children: [
-                  const Align(
-                    alignment: AlignmentDirectional(0, 0),
-                    child: Text(
-                      'Comments',
-                      textAlign: TextAlign.center,
+          builder: (context) {
+            return Padding(
+              padding: EdgeInsets.only(
+                  bottom: MediaQuery.of(context).viewInsets.bottom),
+              child: SafeArea(
+                child: Column(
+                  children: [
+                    const Align(
+                      alignment: AlignmentDirectional(0, 0),
+                      child: Text(
+                        'Comments',
+                        textAlign: TextAlign.center,
+                      ),
                     ),
-                  ),
-                  Expanded(
-                    child: StreamBuilder(
-                      stream: FirebaseFirestore.instance
-                          .collection('posts')
-                          .doc(widget.snap['postId'])
-                          .collection('comments')
-                          .snapshots(),
-                      builder: (context, snapshot) {
-                        if (snapshot.connectionState ==
-                            ConnectionState.waiting) {
-                          return const Center(
-                            child: CircularProgressIndicator(),
+                    const Divider(
+                      height: 3,
+                    ),
+                    Expanded(
+                      child: StreamBuilder(
+                        stream: FirebaseFirestore.instance
+                            .collection('posts')
+                            .doc(widget.snap['postId'])
+                            .collection('comments')
+                            .snapshots(),
+                        builder: (context, snapshot) {
+                          if (snapshot.connectionState ==
+                              ConnectionState.waiting) {
+                            return const Center(
+                              child: CircularProgressIndicator(),
+                            );
+                          }
+                          return ListView.builder(
+                            itemCount: (snapshot.data! as dynamic).docs.length,
+                            itemBuilder: (context, index) => CommentCard(
+                                snap: (snapshot.data! as dynamic)
+                                    .docs[index]
+                                    .data()),
                           );
-                        }
-                        return ListView.builder(
-                          itemCount: (snapshot.data! as dynamic).docs.length,
-                          itemBuilder: (context, index) => CommentCard(
-                              snap: (snapshot.data! as dynamic)
-                                  .docs[index]
-                                  .data()),
-                        );
-                      },
+                        },
+                      ),
                     ),
-                  ),
-                  SingleChildScrollView(
-                    child: Row(
-                      children: [
-                        Padding(
-                          padding:
-                              const EdgeInsetsDirectional.fromSTEB(10, 5, 5, 5),
-                          child: Container(
-                            width: 40,
-                            height: 40,
-                            clipBehavior: Clip.antiAlias,
-                            decoration: const BoxDecoration(
-                              shape: BoxShape.circle,
-                            ),
-                            child: Image.network(
-                              user.photoUrl,
-                              fit: BoxFit.cover,
-                            ),
-                          ),
-                        ),
-                        Expanded(
-                          child: Padding(
-                            padding: const EdgeInsets.only(left: 8, right: 8),
-                            child: TextField(
-                              controller: _commentController,
-                              decoration: InputDecoration(
-                                hintText: 'Comment as ${user.username}',
-                                border: InputBorder.none,
+                    SingleChildScrollView(
+                      child: Row(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsetsDirectional.fromSTEB(
+                                10, 5, 5, 5),
+                            child: Container(
+                              width: 40,
+                              height: 40,
+                              clipBehavior: Clip.antiAlias,
+                              decoration: const BoxDecoration(
+                                shape: BoxShape.circle,
+                              ),
+                              child: Image.network(
+                                user.photoUrl,
+                                fit: BoxFit.cover,
                               ),
                             ),
                           ),
-                        ),
-                        InkWell(
-                          onTap: () async {
-                            FirestoreMethods().postComment(
-                              widget.snap['postId'],
-                              _commentController.text,
-                              user.uid,
-                              user.username,
-                              user.photoUrl,
-                            );
-                            setState(() {
-                              _commentController.text = "";
-                            });
-                          },
-                          child: Container(
-                            padding: const EdgeInsets.symmetric(
-                                vertical: 8, horizontal: 8),
-                            child: const Text('Post'),
+                          Expanded(
+                            child: Padding(
+                              padding: const EdgeInsets.only(left: 8, right: 8),
+                              child: TextField(
+                                controller: _commentController,
+                                decoration: InputDecoration(
+                                  hintText: 'Comment as ${user.username}',
+                                  border: InputBorder.none,
+                                ),
+                              ),
+                            ),
                           ),
-                        )
-                      ],
+                          InkWell(
+                            onTap: () async {
+                              FirestoreMethods().postComment(
+                                widget.snap['postId'],
+                                _commentController.text,
+                                user.uid,
+                                user.username,
+                                user.photoUrl,
+                              );
+                              setState(() {
+                                _commentController.text = "";
+                              });
+                            },
+                            child: Container(
+                              padding: const EdgeInsets.symmetric(
+                                  vertical: 8, horizontal: 8),
+                              child: const Text('Post'),
+                            ),
+                          )
+                        ],
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             );
           },
